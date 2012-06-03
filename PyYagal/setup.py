4171228eb3f309b2fbd797a6ba446ccd0d7ed88f
Hung Dinh <hdinh@ubuntu.(none)>
2012-06-03 11:43:16 -0700

    still fighting with setup.py

--- PyYagal/setup.py
+++ PyYagal/setup.py
@@ -1,14 +1,19 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+#from Cython.Distutils import build_ext
+
+from setuptools import find_packages
 
 import os.path
 script_dir = os.path.dirname(__file__)
 
-ext_modules = [Extension("yagal", [os.path.join(script_dir, "yagal.pyx")])]
+#ext_modules = [Extension("yagal", [os.path.join(script_dir, "yagal.pyx")])]
+#ext_modules = [Extension("yagal")]
 
 setup(
-  name = 'Yagal',
-  cmdclass = {'build_ext': build_ext},
-  ext_modules = ext_modules
+  name = 'yagal',
+  #cmdclass = {'build_ext': build_ext},
+  #ext_modules = ext_modules,
+  url="https://github.com/hdinh/yagal",
+  packages = find_packages(),
 )
